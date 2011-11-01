f911e54179793311b1980fcabfbc419eb6de9a8a
Ming Jin <skyairmj@gmail.com>
2011-11-01 23:28:34 +0800

    remove cramp settings. use sinatra for sync part, cramp for async part

--- app/actions/home_action.rb
+++
@@ -1,6 +0,0 @@
-class HomeAction < Cramp::Action
-  def start
-    render "Hello World!"
-    finish
-  end
-end
--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-# Check out https://github.com/joshbuddy/http_router for more information on HttpRouter
-HttpRouter.new do
-  add('/').to(HomeAction)
-end
