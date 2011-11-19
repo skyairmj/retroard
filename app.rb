649ebc4f507f9c78f3cf14c99723f540719bbbbd
Ming Jin <skyairmj@gmail.com>
2011-11-19 10:45:41 +0800

    Ming Jin: polished UI and add team profile link

--- app.rb
+++ app.rb
@@ -7,11 +7,6 @@ get '/' do
   erb :index
 end
 
-get '/:team' do
-  #"hello #{params[:team]}"
-  erb :board
-end
-
 post '/signin' do
   unless params[:team].nil?
     redirect "/#{params[:team]}", 303
@@ -22,4 +17,13 @@ post '/signup' do
   unless params[:team].nil?
     redirect "/#{params[:team]}", 303
   end
+end
+
+get '/:team' do
+  #"hello #{params[:team]}"
+  erb :board                                                                                                                     
+end
+
+get '/:team/profile' do
+  "hello! #{params[:team]}"
 end
