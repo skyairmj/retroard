8a7df1fbf91e3c2a28f2100bcf1894fe7eae8ce6
Ming Jin <skyairmj@gmail.com>
2011-11-29 00:57:52 +0800

    Ming Jin: wrap the redis connection & methods

---
+++ redis_connection.rb
@@ -0,0 +1,14 @@
+require 'redis'
+
+class RedisConnection
+
+  def initialize host=nil, port=nil
+    host = (host || ENV['REDIS_HOST'] || 'localhost')
+    port = (port || ENV['REDIS_PORT'] || 6379).to_i
+    @redis = Redis.new(:host => host, :port => port)
+  end
+
+  def method_missing(method, *args, &block)
+    @redis.send method, *args
+  end
+end
